dc5f630c37039b1416f4e7abc0b72b21323d7e96
andromeda <you@example.com>
2023-11-07 12:12:21 +0300

    add files

--- login_logout.py
+++ login_logout.py
@@ -7,89 +7,95 @@ import mortal_combat
 from colorama import Fore
 from progress.bar import PixelBar
 import time
+
+
 def progress_bar():
-    bar = PixelBar('Загрузка:', max= 40)
+    bar = PixelBar("Загрузка:", max=40)
     for i in range(40):
         time.sleep(0.01)
         bar.next()
     bar.finish()
 
 
-
 def read_file(filename):
-    with open(filename, 'r', encoding='utf-8') as file:
-      return file.read().split('\n')
-  
+    with open(filename, "r", encoding="utf-8") as file:
+        return file.read().split("\n")
+
+
 def sign_in():
-    login_list = read_file('logins.txt')
-    password_list = read_file('passwords.txt')
-    login = input('Введите ваш логин: ')
-    password = input('Введите ваш пароль: ')
+    login_list = read_file("logins.txt")
+    password_list = read_file("passwords.txt")
+    login = input("Введите ваш логин: ")
+    password = input("Введите ваш пароль: ")
     flag = False
     if login in login_list and password in password_list:
         flag = True
     return flag, login
 
+
 def sign_up():
-    print('Давайте мы вас зарегистрируем: ')
-    login = input('Придумайте логин: ')
-    password1 = input('Создайте пароль: ')
-    password2 = input('Повторите пароль: ')
+    print("Давайте мы вас зарегистрируем: ")
+    login = input("Придумайте логин: ")
+    password1 = input("Создайте пароль: ")
+    password2 = input("Повторите пароль: ")
     if password1 == password2:
-        with open('logins.txt', 'a', encoding='utf-8') as file:
-            file.write(f'{login}\n')
-        with open('passwords.txt', 'a', encoding='utf-8') as file:
-            file.write(f'{password1}\n')
-        print('вы успешно зарегистрированы')
-        
+        with open("logins.txt", "a", encoding="utf-8") as file:
+            file.write(f"{login}\n")
+        with open("passwords.txt", "a", encoding="utf-8") as file:
+            file.write(f"{password1}\n")
+        print("вы успешно зарегистрированы")
+
+
 def sign_out():
-    print('Вы вышли из системы')
+    print("Вы вышли из системы")
     flag, login = False, False
     return flag, login
+
 
 flag = False
 while True:
     progress_bar()
     if not flag:
-        do = input("""
+        do = input(
+            """
                Что вы хотите сделать?
                1 - войти в аккаунт
                2 - зарегистрироваться
                3 - выйти из программы
-               """)
-        if do == '1':
+               """
+        )
+        if do == "1":
             flag, login = sign_in()
-        elif do == '2':
+        elif do == "2":
             sign_up()
-        elif do == '3':
-            print('Спасибо за использование программы')
+        elif do == "3":
+            print("Спасибо за использование программы")
             break
         else:
-            print('Такого варианта не существует')
-    else:     
-        print(Fore.RED + '1 - выйти из аккаунта')
-        print(Fore.BLACK + '2 - выйти из программы')
-        print(Fore.GREEN + '3 - запустить калькулятор')
-        print(Fore.YELLOW + '4 - запустить РоботКрафт')
-        print(Fore.BLUE + '5 - открыть список желаний')
+            print("Такого варианта не существует")
+    else:
+        print(Fore.RED + "1 - выйти из аккаунта")
+        print(Fore.BLACK + "2 - выйти из программы")
+        print(Fore.GREEN + "3 - запустить калькулятор")
+        print(Fore.YELLOW + "4 - запустить РоботКрафт")
+        print(Fore.BLUE + "5 - открыть список желаний")
         print(Fore.MAGENTA + '6 - поиграть в "Угадай число"')
         print(Fore.CYAN + '7 - поиграть в "Быки и коровы"')
         do = input(Fore.RESET + "Что вы хотите сделать?")
-        if do == '1':
+        if do == "1":
             flag, login = sign_out()
-        elif do == '2':
-            print('Спасибо за использование программы')
+        elif do == "2":
+            print("Спасибо за использование программы")
             break
-        elif do == '3':
+        elif do == "3":
             calc.calc()
-        elif do == '4':
+        elif do == "4":
             robot.main()
-        elif do == '5':
+        elif do == "5":
             wish_list.wish_app()
-        elif do == '6':
+        elif do == "6":
             guess_number.game()
-        elif do == '7':
+        elif do == "7":
             cows_bulls.cb_game(login)
-        elif do == '8':
+        elif do == "8":
             mortal_combat.fighting()
-        
